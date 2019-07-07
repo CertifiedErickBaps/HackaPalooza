6ee6fe9d2f9bc7b79df7df110fcba86ff0111c00
Ericksad01 <erickburn01@hotmail.com>
2019-07-06 22:29:45 -0500

    Agregacion de public y server

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+const path = require("path");
+
+// habilitar carpeta public
+app.use(express.static(path.resolve(__dirname, "../public")));
+
+app.listen(8080, () => {
+  console.log("Escuchando puerto: ", 8080);
+});
